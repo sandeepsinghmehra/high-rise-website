642e475111ebb63833a27dccfc4855135cb5d9b4
sandeepsinghmehra <sandysmart017@gmail.com>
2024-05-24 14:33:13 +0530

    Added twitter og tags for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ export const metadata: Metadata = {
     default: "Highrise Construction Solutions", // a default is required when creating a template
   },
   description: "We are a construction company with over 25 years of experience in the Construction Management and General Contracting domain specializing in building skyscrapers. Our extensive experience, professionally skilled and eager staff and our unwavering commitment to excellence enables us to deliver high quality projects on time and monitoring budget within set constraints.",
+  twitter: {
+    card: "summary_large_image"
+  },
+  // openGraph: {
+
+  // }
 };
 
 export default function RootLayout({
