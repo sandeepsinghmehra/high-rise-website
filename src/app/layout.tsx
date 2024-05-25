c9932304bc19f55389e15e800672bcc5a876561f
sandeepsinghmehra <sandysmart017@gmail.com>
2024-05-25 08:22:58 +0530

    Added favicon icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,42 @@ export const metadata: Metadata = {
   // openGraph: {
 
   // }
+  // icons: 'favicon.ico',
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        type: "image/ico",
+        url: "/favicon/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        url: "/favicon/android-chrome-192x192.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "512x512",
+        url: "/favicon/android-chrome-512x512.png",
+      },
+    ],
+  },
+
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
